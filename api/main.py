17d63e5c184c9198625d4fe7760dde7a2977f9fd
Cuauhtemoc Núñez Valencia <cuau_valencia@hotmail.com>
2025-09-02 14:11:38 -0700

    Create main.py

---
+++ api/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+
+
+app = FastAPI()
+model = joblib.load("models/prophet_model.pkl")
+
+
+@app.get("/forecast")
+def get_forecast(days: int = 30):
+future = model.make_future_dataframe(periods=days)
+forecast = model.predict(future)
+return forecast[['ds','yhat','yhat_lower','yhat_upper']].tail(days).to_dict(orient="records")
