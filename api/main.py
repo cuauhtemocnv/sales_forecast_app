bfcdfc2fc87f289fd54bbe0c93f4eb63852d8e16
Cuauhtemoc Núñez Valencia <cuau_valencia@hotmail.com>
2025-09-02 14:42:41 -0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -9,6 +9,6 @@ model = joblib.load("models/prophet_model.pkl")
 
 @app.get("/forecast")
 def get_forecast(days: int = 30):
-future = model.make_future_dataframe(periods=days)
-forecast = model.predict(future)
-return forecast[['ds','yhat','yhat_lower','yhat_upper']].tail(days).to_dict(orient="records")
+  future = model.make_future_dataframe(periods=days)
+  forecast = model.predict(future)
+  return forecast[['ds','yhat','yhat_lower','yhat_upper']].tail(days).to_dict(orient="records")
